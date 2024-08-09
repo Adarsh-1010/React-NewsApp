154477336a50352f4e92db4a0c0175f9d8b0df78
Adarsh-1010 <adarsh.21ug3009@iiitranchi.ac.in>
2024-08-09 23:09:36 +0530

    added news items and news

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+import News from './Components/News'
 
 export default class App extends Component {
-  c="adarsh";
+
   render() {
     return (
       <div>
-        Hello this is my first class based component {this.c}
+        <Navbar/>
+        <News/> 
       </div> 
     )
   }

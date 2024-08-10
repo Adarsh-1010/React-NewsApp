39bca869bc26b2278a0f4abcd8efa03d34640cd0
Adarsh-1010 <adarsh.21ug3009@iiitranchi.ac.in>
2024-08-11 00:57:07 +0530

    added output json file

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
-export default class NewsItem extends Component {
+export default class NewsItem extends Component { 
+
+      
   render() {
+    let {title,description,imgUrl}=this.props;
+
     return (
-      <div>
-        This is a news Item Component
+
+      <div className='my-3'>  
+        <div className="card" style={{width: "18rem"}}>
+        <img src={imgUrl} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+        </div>
+      </div>
       </div>
     )
   }
